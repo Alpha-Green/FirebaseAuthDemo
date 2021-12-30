bde746b619f2a5f4d39cba602e20109bed87f415
Alpha-Green <erabornoah@gmail.com>
2021-12-30 10:57:35 +0100

    My first commit

---
+++ app/src/main/java/com/noahgreen/firebaseauth/views/HomeActivity.kt
@@ -0,0 +1,26 @@
+package com.noahgreen.firebaseauth.views
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.noahgreen.firebaseauth.R
+import com.noahgreen.firebaseauth.extensions.Extensions.toast
+import com.noahgreen.firebaseauth.utils.FirebaseUtils.firebaseAuth
+import kotlinx.android.synthetic.main.activity_home.*
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        // sign out a user
+
+        btnSignOut.setOnClickListener {
+            firebaseAuth.signOut()
+            startActivity(Intent(this, CreateAccountActivity::class.java))
+            toast("signed out")
+            finish()
+        }
+    }
+}
+
